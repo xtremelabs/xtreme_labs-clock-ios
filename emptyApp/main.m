9be11cc5065cb9d8125e743cb6ad3a3064d93957
MichelleDH/RyanC <michelle@xtremelabs.com>
2013-02-06 18:20:56 -0500

    first commit

---
+++ emptyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  emptyApp
+//
+//  Created by DX 108 on 2013-02-06.
+//  Copyright (c) 2013 Xtreme Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
